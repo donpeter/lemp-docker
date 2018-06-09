18446adb3927d9e3b357e0877b961d71a9b1394c
Don Peter C. Atunalu <patunalu@yahoo.com>
2018-06-09 13:56:18 +0300

    Adding PHP

--- app/index.php
+++ app/index.php
@@ -1,8 +1,9 @@
+<?php $title = "LEMP (Docker) - Code with Don Peter"?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>LEMP (Docker) - Code with Don Peter</title>
+    <title><?= $title?></title>
     <style>
         body{
             text-align: center;
@@ -10,7 +11,8 @@
     </style>
 </head>
 <body>
-    <h1>LEMP (Docker) - Code with Don Peter</h1>
+    <h1><?= $title?></h1>
     <p>This page is been server by Nginx in a docker container </p>
+    <?php echo "According to PHP today's date and time is <strong>" . date("D jS M, Y h:i:sa")."</strong>"?>
 </body>
 </html>
