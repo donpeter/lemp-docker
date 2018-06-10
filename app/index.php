6cbd5c95dbd087fb5f24e9bd5ed4f1488bc9f394
Don Peter C. Atunalu <patunalu@yahoo.com>
2018-06-10 06:29:48 +0300

    Rename docker-compose services

--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 $title = "LEMP (Docker) - Code with Don Peter";
-$host = "db"; // Database host               -> Same as service name in the docker-compose file
+$host = "mysql"; // Database host               -> Same as service name in the docker-compose file
 $dbname= "lemp-db"; // Database name         -> Set as an enviroment variable in the docker-compose file
 $user = "lemp-user"; // Database user        -> Set as an enviroment variable in the docker-compose file
 $pass = "lemp-pass"; // Database password    -> Set as an enviroment variable in the docker-compose file
@@ -8,10 +8,13 @@ $pass = "lemp-pass"; // Database password    -> Set as an enviroment variable in
 // Create connection
 try {
     $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $db_conn =  "Database Connected!!!";
+    $db_status = true;
     $dbh = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+    $db_conn =  "Database Not Connected!!!";
+    $db_status = false;
 }
 ?>
 <!DOCTYPE html>
@@ -23,6 +26,9 @@ try {
         body{
             text-align: center;
         }
+        #db{
+            color: <?= $db_status ? "green": "red"?>;
+        }
     </style>
 </head>
 <body>
@@ -30,6 +36,7 @@ try {
     <p>This page is been server by Nginx in a docker container </p>
     <?php echo "According to PHP today's date and time is <strong>" . date("D jS M, Y h:i:sa")."</strong>"?>
     <br>
-    <h1> Database Connected!!!
+    <h1 id="db"><?= $db_conn?> </h1>
+    <p>
 </body>
 </html>
